0866e17a245178d731bfee56d566b9010f0b6d10
TegHocine <61586802+TegHocine@users.noreply.github.com>
2025-02-07 19:45:36 +0100

    update add/detail product

--- front/src/app/pages/product-page/product-page.component.ts
+++ front/src/app/pages/product-page/product-page.component.ts
@@ -2,7 +2,6 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { MessageService } from 'primeng/api';
 
-import { catchError, of, take } from 'rxjs';
 import { ProductDetailComponent } from '../../components/product-detail/product-detail.component';
 import { ProductFormComponent } from '../../components/product-form/product-form.component';
 import { ProductListComponent } from '../../components/product-list/product-list.component';
@@ -10,7 +9,7 @@ import { SearchProductComponent } from '../../components/search-product/search-p
 import { CategoryService } from '../../services/category.service';
 import { ProductService } from '../../services/product.service';
 import { Category } from '../../types/category.type';
-import { CreateProduct, Product } from '../../types/product.type';
+import { Product } from '../../types/product.type';
 
 @Component({
   selector: 'app-product-page',
@@ -57,33 +56,5 @@ export class ProductPage {
   onEdit(product: Product) {
     this.selectedProduct = product;
     this.onShow();
-  }
-
-  onUpdate(newProduct: CreateProduct) {
-    this.productService
-      .update(newProduct)
-      .pipe(
-        take(1),
-        catchError(() => {
-          this.messageService.add({
-            severity: 'error',
-            summary: 'Erreur',
-            detail:
-              'Erreur lors de la modification du produit. Veuillez réessayer.',
-            life: 3000,
-          });
-          this.onClose();
-          return of(null);
-        })
-      )
-      .subscribe(() => {
-        this.onClose();
-        this.messageService.add({
-          severity: 'success',
-          summary: 'Succès',
-          detail: 'Produit modifié avec succès.',
-          life: 3000,
-        });
-      });
   }
 }
